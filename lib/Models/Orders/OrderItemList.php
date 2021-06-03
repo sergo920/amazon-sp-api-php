576044b01181ffa1c7a307c3a7c628cd84a9792f
Sergey <sergo920@yahoo.com>
2021-06-03 21:09:29 +0300

    Update OrderItemList.php

--- lib/Models/Orders/OrderItemList.php
+++ lib/Models/Orders/OrderItemList.php
@@ -167,9 +167,10 @@ class OrderItemList implements ModelInterface, ArrayAccess, IterableType
      */
     public function listInvalidProperties()
     {
-        $invalidProperties = parent::listInvalidProperties();
+        return [];
+       // $invalidProperties = parent::listInvalidProperties();
 
-        return $invalidProperties;
+        //return $invalidProperties;
     }
 
     /**
