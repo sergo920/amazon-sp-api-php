e060bc0f417779d3347a86f00c4790416496da9a
Sergey <sergo920@yahoo.com>
2021-06-03 21:11:12 +0300

    Update PaymentMethodDetailItemList.php

--- lib/Models/Orders/PaymentMethodDetailItemList.php
+++ lib/Models/Orders/PaymentMethodDetailItemList.php
@@ -166,9 +166,10 @@ class PaymentMethodDetailItemList implements ModelInterface, ArrayAccess
      */
     public function listInvalidProperties()
     {
-        $invalidProperties = parent::listInvalidProperties();
+        return [];
+//         $invalidProperties = parent::listInvalidProperties();
 
-        return $invalidProperties;
+//         return $invalidProperties;
     }
 
     /**
