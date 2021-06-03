95751a27927d355ed451214cad7b3bef25fff610
Sergey <sergo920@yahoo.com>
2021-06-03 21:12:30 +0300

    Update DestinationList.php

--- lib/Models/Notifications/DestinationList.php
+++ lib/Models/Notifications/DestinationList.php
@@ -167,9 +167,10 @@ class DestinationList implements ModelInterface, ArrayAccess, IterableType
      */
     public function listInvalidProperties()
     {
-        $invalidProperties = parent::listInvalidProperties();
+        return [];
+//         $invalidProperties = parent::listInvalidProperties();
 
-        return $invalidProperties;
+//         return $invalidProperties;
     }
 
     /**
