63cd698f72b8477e573adf9fd6fe4cade95251d8
Sergey <sergo920@yahoo.com>
2021-07-20 14:10:46 +0300

    Update AttributeSetList.php

--- lib/Models/ProductPricing/AttributeSetList.php
+++ lib/Models/ProductPricing/AttributeSetList.php
@@ -166,9 +166,9 @@ class AttributeSetList implements ModelInterface, ArrayAccess
      */
     public function listInvalidProperties()
     {
-        $invalidProperties = parent::listInvalidProperties();
+//         $invalidProperties = parent::listInvalidProperties();
 
-        return $invalidProperties;
+        return [];
     }
 
     /**
