d1a23b33171b246fffe2b013cfc3e87ec95d34d5
Sergey <sergo920@yahoo.com>
2021-07-20 14:13:20 +0300

    Update OfferDetailList.php

--- lib/Models/ProductPricing/OfferDetailList.php
+++ lib/Models/ProductPricing/OfferDetailList.php
@@ -164,9 +164,9 @@ class OfferDetailList implements ModelInterface, ArrayAccess, IterableType
      */
     public function listInvalidProperties()
     {
-        $invalidProperties = parent::listInvalidProperties();
+//         $invalidProperties = parent::listInvalidProperties();
 
-        return $invalidProperties;
+        return [];
     }
 
     /**
